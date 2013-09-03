581a7a78532d9fa9f475441de11356b2e7e6e1c8
Mayoto Yoshida <2getjp@gmail.com>
2013-09-04 00:34:08 +0900

    Add scss

--- app.rb
+++ app.rb
@@ -7,10 +7,19 @@ module Project
       Bundler.require :development
       register Sinatra::Reloader
       Slim::Engine.set_default_options :pretty => true
+      set :scss, :views => 'assets/scss', :style => :expanded
+    end
+
+    configure :production do
+      set :scss, :views => 'assets/scss', :style => :compressed
     end
 
     before do
       @site_title = 'Site Title'
+    end
+
+    get '/css/:name.css' do
+      scss params[:name].to_sym
     end
 
     get '/' do
